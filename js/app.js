20268a92215caeb75a1e14eba608448710528ddc
Miguel Cobian <miguelcobian99@gmail.com>
2023-04-22 12:59:25 -0600

    Función para Mostrar Error agregada

--- js/app.js
+++ js/app.js
@@ -13,6 +13,32 @@ function eventListeners() {
 function agregarTweet(e) {
     e.preventDefault();
 
-    console.log('Agregando tweet');
-}
+    // Textarea dond eel usuario escribe
+    const tweet = document.querySelector('#tweet').value;
+    // Validacion
+    if (tweet === '') {
+        mostrarError('El campo no puede estar vacío');
+        return;
+    }
+    console.log('Agregando tweet');   
+};
 
+
+
+//Mostrar Mensaje de Error
+function mostrarError(error) {
+    const mensajeError =document.createElement('p');
+    mensajeError.textContent = error;
+    mensajeError.classList.add('error');
+
+    // Insertar en el contenido
+    const contenido = document.querySelector('#contenido');
+    contenido.appendChild(mensajeError);
+
+
+    //Elimina la alerta después de tres segundos
+    setTimeout(() => {
+        mensajeError.remove();
+    }, 3000);
+
+};
